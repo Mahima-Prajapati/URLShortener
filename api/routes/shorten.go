85f64ad19e28c6db2488f1bf1deb34110c7ec1e8
Mahima-Prajapati <118904414+Mahima-Prajapati@users.noreply.github.com>
2025-08-19 17:36:10 +0000

    Modified shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/Mahima-Prajapati/URLShortener/database"
 	"github.com/Mahima-Prajapati/URLShortener/helpers"
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -30,6 +35,23 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// implement rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":           "Rate limit exceeded",
+				"rate_limit_rset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	// check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
@@ -44,4 +66,34 @@ func ShortenURL(c *fiber.Ctx) error {
 	// enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
 
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "URL custom short is already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 20
+	}
+
+	errs := r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Minute)
+	if errs != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "unable to connect to server",
+		})
+	}
+
+	r2.Decr(database.Ctx, c.IP())
 }
